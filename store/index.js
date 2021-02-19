fc54a3328b15689f75135af4ad714dfce1b03f03
Michael B <14895993+belozerskiy@users.noreply.github.com>
2021-02-19 13:17:22 +0300

    remove comments

--- store/index.js
+++ store/index.js
@@ -1,7 +1,1 @@
-// import { callPlugin } from '@/plugins/storePlugins'
 
-// export const plugins = [callPlugin]
-
-// export const FETCH_CALL_LIST_ACTION = 'FETCH_CALL_LIST_ACTION'
-
-// export const actions = {}
